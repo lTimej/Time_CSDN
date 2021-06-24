d9ea21d6126e395c4223ba409ccc09f0d8aa2631
liujun <liujun19950425@yeah.net>
2021-06-24 14:05:01 +0800

    '搜索自动补全功能'

--- csdn/resources/search/__init__.py
+++ csdn/resources/search/__init__.py
@@ -1,12 +1,16 @@
 from flask import Blueprint
 from flask_restful import Api
 from utils.output import output_json
-from . import searchs,history
+from . import searchs,history,suggests
 
 bp_search = Blueprint("search",__name__)
 
 search_api = Api(bp_search,catch_all_404s=True)
 search_api.representation('application/json')(output_json)
 
+#文章搜索内容
 search_api.add_resource(searchs.AllSearchView,"/v1/user/search",endpoint="search")
+#用户搜索历史
 search_api.add_resource(history.UserHistorySearchRecord,"/v1/search/history",endpoint="sHistory")
+#搜索建议
+search_api.add_resource(suggests.UserSearchSuggest,"/v1/search/suggest",endpoint="suggest")
--- csdn/resources/search/suggests.py
+++ csdn/resources/search/suggests.py
@@ -8,6 +8,65 @@ class UserSearchSuggest(Resource):
     '''
     搜索建议
     '''
+    def select(self,keyword):
+        '''
+        从elasticsearch查询
+        :param keyword:
+        :return:
+        '''
+        query = {
+            'from': 0,
+            'size': 20,
+            '_source': False,
+            'suggest': {
+                'word-completion': {
+                    'prefix': keyword,
+                    'completion': {
+                        'field': 'suggest'
+                    }
+                }
+            }
+        }
+        #建议
+        res = current_app.es.search(index='completions', body=query)
+        options = res['suggest']['word-completion'][0]['options']
+        return options
     def get(self):
+        '''
+        获取建议词条
+        :return:
+        '''
+        #获取关键字
         data = RequestParser()
-        data.add_argument("keyword", type=inputs.regex(r'^.{1,50}$'), required=True, location='args')
+        #校验关键字
+        data.add_argument("keyword", type=inputs.regex(r'^.{0,50}$'), required=True, location='args')
+        args = data.parse_args()
+        keyword = args.keyword
+        #获取建议文章
+        options = self.select(keyword)
+        #为空，进行词条纠错
+        if not options:
+            query = {
+                'from': 0,
+                'size': 20,
+                '_source': False,
+                'suggest': {
+                    'text': keyword,
+                    'word-phrase': {
+                        'phrase': {
+                            'field': 'title',
+                            'size': 1
+                        }
+                    }
+                }
+            }
+            ret = current_app.es.search(index='articles', doc_type='article', body=query)
+            options = ret['suggest']['word-phrase'][0]['options']
+            ky = options[0].get("text")
+            #纠错后在查询
+            options = self.select(ky)
+        searchs = []
+        for option in options:
+            if option.get("text") not in searchs:
+                searchs.append(option.get("text"))
+        return {"message":"ok","searchs":searchs},201
